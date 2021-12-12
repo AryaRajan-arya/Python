d69555724fc57b14b3fb0cc2b99573694d4d174f
AryaRajan-arya <aryarajan074@gmail.com>
2021-12-12 20:59:50 +0530

    file added

---
+++ CO1/CO1_1.py
@@ -0,0 +1,4 @@
+#1. Familiarizing Text Editor
+print("hello")
+x=int(input("enter a number:"))
+print(x)
---
+++ CO1/CO1_9.py
@@ -0,0 +1,3 @@
+#Create a string from given string where first and last characters exchanged. [eg: python -> nythop
+str=input("enter a string=")
+print(str[-1]+str[1:-1]+str[0])
